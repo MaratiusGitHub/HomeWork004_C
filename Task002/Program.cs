f34b0b346b874eaa36c80dae0170865aeec4c7d9
Maratius <maratius.ru@gmail.com>
2022-08-18 23:36:29 +0300

    task002

---
+++ Task002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+double result = 0;
+
+while (a != 0)
+{
+    result = result + a % 10;
+    a = a / 10;
+}
+Console.WriteLine(result);
+
